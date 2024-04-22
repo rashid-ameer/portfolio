27f8e86063f915f5e936e93cf12d94df0f657896
Rashid Ameer <r.ameer.offical@gmail.com>
2024-04-22 20:45:13 +0500

    Fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,61 +6,57 @@ import useMotion from "../hooks/Motion";
 import { motion } from "framer-motion";
 
 const Footer = () => {
-	const { swipe, motionRef: motionRef1, controls: control1 } = useMotion();
-	const { motionRef: motionRef2, controls: control2 } = useMotion();
-	const { motionRef: motionRef3, controls: control3 } = useMotion();
+  const { swipe, motionRef: motionRef1, controls: control1 } = useMotion();
+  const { motionRef: motionRef2, controls: control2 } = useMotion();
+  const { motionRef: motionRef3, controls: control3 } = useMotion();
 
-	return (
-		<Container styleClasses={"text-center space-y-7"}>
-			<motion.h2
-				initial={{ y: "100%" }}
-				animate={control1}
-				transition={{ duration: 0.5 }}
-				ref={motionRef1}
-				variants={swipe("y", "100%")}
-				className="text-3xl xl:text-4xl text-dark-200 font-medium"
-			>
-				Let's Code Something
-				<br /> Amazing Together
-			</motion.h2>
-			<motion.p
-				initial={{ y: "100%" }}
-				animate={control2}
-				transition={{ duration: 1 }}
-				ref={motionRef2}
-				variants={swipe("y", "100%")}
-			>
-				For development needs, inquiries, or a friendly connection,
-				reach out anytime
-			</motion.p>
+  return (
+    <Container styleClasses={"text-center space-y-7"}>
+      <motion.h2
+        initial={{ y: "100%" }}
+        animate={control1}
+        transition={{ duration: 0.5 }}
+        ref={motionRef1}
+        variants={swipe("y", "100%")}
+        className="text-3xl xl:text-4xl text-dark-200 font-medium">
+        Let's Code Something
+        <br /> Amazing Together
+      </motion.h2>
+      <motion.p
+        initial={{ y: "100%" }}
+        animate={control2}
+        transition={{ duration: 1 }}
+        ref={motionRef2}
+        variants={swipe("y", "100%")}>
+        For development needs, inquiries, or a friendly connection, reach out anytime
+      </motion.p>
 
-			<motion.div
-				initial={{ y: "100%" }}
-				animate={control3}
-				transition={{ duration: 1 }}
-				ref={motionRef3}
-				variants={swipe("y", "100%")}
-				id="social-links"
-				className="flex items-center justify-center gap-12 text-base text-dark-200"
-			>
-				<FooterSocial
-					link={"https://github.com/RA-Offical"}
-					title="Github"
-					Icon={GithubIcon}
-				/>
-				<FooterSocial
-					link={"https://www.linkedin.com/in/rashid-ameer-b30aa6241"}
-					title="LinkedIn"
-					Icon={LinkedInIcon}
-				/>
-				<FooterSocial
-					link={"mailto:r.ameer.offical@gamil.com"}
-					title={"Mail"}
-					Icon={Mail}
-				/>
-			</motion.div>
-		</Container>
-	);
+      <motion.div
+        initial={{ y: "100%" }}
+        animate={control3}
+        transition={{ duration: 1 }}
+        ref={motionRef3}
+        variants={swipe("y", "100%")}
+        id="social-links"
+        className="flex items-center justify-center gap-12 text-base text-dark-200">
+        <FooterSocial
+          link={"https://github.com/rashid-ameer"}
+          title="Github"
+          Icon={GithubIcon}
+        />
+        <FooterSocial
+          link={"https://www.linkedin.com/in/rashid-ameer-b30aa6241"}
+          title="LinkedIn"
+          Icon={LinkedInIcon}
+        />
+        <FooterSocial
+          link={"mailto:r.ameer.offical@gamil.com"}
+          title={"Mail"}
+          Icon={Mail}
+        />
+      </motion.div>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,78 +4,78 @@ import { profile2 } from "../assets/images";
 import { motion } from "framer-motion";
 
 const Hero = () => {
-	return (
-		<div className="relative h-dynamic-full max-h-[900px] bg-blue-50] overflow-hidden">
-			{/* ts particles */}
-			{/* <TsParticles /> */}
+  return (
+    <div className="relative h-dynamic-full max-h-[900px] bg-blue-50] overflow-hidden">
+      {/* ts particles */}
+      {/* <TsParticles /> */}
 
-			<Container
-				styleClasses={
-					"grid grid-rows-2 lg:grid-rows-none lg:grid-cols-2 justify-items-center lg:justify-items-start gap-6 h-full pt-12 lg:pt-0"
-				}>
-				{/* introduction */}
-				<motion.div
-					initial={{ y: 100 }}
-					animate={{ y: 0 }}
-					transition={{ duration: 0.8 }}
-					className="text-center lg:text-left space-y-6 self-center">
-					<h1
-						initial={{ y: 100 }}
-						animate={{ y: 0 }}
-						transition={{ duration: 1 }}
-						className="text-5xl xl:text-6xl font-bold text-blue">
-						<span className="text-dark-200">
-							Hello{" "}
-							<motion.span
-								animate={{ rotate: 15 }}
-								transition={{
-									ease: "linear",
-									duration: 1,
-									repeat: Infinity,
-									repeatType: "reverse",
-									repeatDelay: 0.25,
-								}}
-								className="inline-block">
-								👋
-							</motion.span>
-							,<br /> I am
-						</span>{" "}
-						Rashid Ameer
-					</h1>
+      <Container
+        styleClasses={
+          "grid grid-rows-2 lg:grid-rows-none lg:grid-cols-2 justify-items-center lg:justify-items-start gap-6 h-full pt-12 lg:pt-0"
+        }>
+        {/* introduction */}
+        <motion.div
+          initial={{ y: 100 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="text-center lg:text-left space-y-6 self-center">
+          <h1
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 1 }}
+            className="text-5xl xl:text-6xl font-bold text-blue">
+            <span className="text-dark-200">
+              Hello{" "}
+              <motion.span
+                animate={{ rotate: 15 }}
+                transition={{
+                  ease: "linear",
+                  duration: 1,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                  repeatDelay: 0.25,
+                }}
+                className="inline-block">
+                👋
+              </motion.span>
+              ,<br /> I am
+            </span>{" "}
+            Rashid Ameer
+          </h1>
 
-					<p className="text-lg xl:text-xl">
-						A dynamic front-end developer with over 1 years of experience in HTML, CSS, and
-						JavaScript, complemented by proficiency in React.
-					</p>
+          <p className="text-lg xl:text-xl">
+            A dynamic front-end developer with over 4 years of experience in HTML, CSS, and JavaScript, complemented by
+            proficiency in React.
+          </p>
 
-					{/* Rounded button for CTA*/}
-					<motion.a
-						whileHover={{ scale: 1.05 }}
-						whileTap={{ scale: 0.95 }}
-						transition={{
-							type: "spring",
-							stiffness: 400,
-							damping: 5,
-						}}
-						href="#social-links"
-						className="group flex gap-2 items-center justify-center p-5 py-2.5 mx-auto lg:mx-0 rounded-full text-white font-medium bg-blue active:bg-blue-300 hover:bg-blue-400 transition-[background] w-fit ">
-						Hire me
-						<ArrowLeft className="rotate-180 transition-transform group-hover:translate-x-1.5" />
-					</motion.a>
-				</motion.div>
+          {/* Rounded button for CTA*/}
+          <motion.a
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{
+              type: "spring",
+              stiffness: 400,
+              damping: 5,
+            }}
+            href="#social-links"
+            className="group flex gap-2 items-center justify-center p-5 py-2.5 mx-auto lg:mx-0 rounded-full text-white font-medium bg-blue active:bg-blue-300 hover:bg-blue-400 transition-[background] w-fit ">
+            Hire me
+            <ArrowLeft className="rotate-180 transition-transform group-hover:translate-x-1.5" />
+          </motion.a>
+        </motion.div>
 
-				{/* profile image */}
-				<motion.img
-					initial={{ y: 100 }}
-					animate={{ y: 0 }}
-					transition={{ duration: 1 }}
-					src={profile2}
-					className="h-full w-auto xl:w-full lg:h-auto object-cover self-end drop-shadow-[0px_0px_10px_rgba(0,0,0,0.3)]"
-					alt="Rashid Ameer"
-				/>
-			</Container>
-		</div>
-	);
+        {/* profile image */}
+        <motion.img
+          initial={{ y: 100 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1 }}
+          src={profile2}
+          className="h-full w-auto xl:w-full lg:h-auto object-cover self-end drop-shadow-[0px_0px_10px_rgba(0,0,0,0.3)]"
+          alt="Rashid Ameer"
+        />
+      </Container>
+    </div>
+  );
 };
 
 export default Hero;
